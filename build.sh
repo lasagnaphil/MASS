8516d0d3d5888aff1273b2eb385a45510b0bf7ae
Phillip Chang <phil@mrl.snu.ac.kr>
2020-09-01 16:07:19 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+ENVDIR=/home/$USER/pkgenv
+
 mkdir -p build
 pushd build
 cmake -DMASS_BUILD_RENDERER=OFF \
       -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$ENVDIR \
       -DCMAKE_INSTALL_PREFIX=/home/$USER/pkgenv \
       -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
       -DCMAKE_INSTALL_RPATH=/home/$USER/pkgenv \
-      -DFCL_INCLUDE_DIRS=/home/$USER/pkgenv/include/fcl \
-      ..
+
 make -j$(nproc)
 popd
